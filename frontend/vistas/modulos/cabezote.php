891e095ef094cd77be80e00e533f57eb0061ce6c
Miguel Gutierrez <miguelangel@MacBook-Air-de-Miguel.local>
2020-08-17 22:48:57 -0400

    Maquetando el cabezote 2

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -93,10 +93,158 @@ HEADER
             </div>
 
             <!--========================================================
-            BLOQUE CATEGORIA Y BUSCADOR
+            BLOQUE CATEGORÍA Y BUSCADOR
             =========================================================-->
 
-            <div class="col-lg-6 col-md-3 col-sm-6 col-xs-12">
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+
+                <!--========================================================
+                BOTÓN CATEGORÍA
+                =========================================================-->
+
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+
+                    <p>CATEGORÍAS
+
+                        <span class="pull-right">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                        </span>
+
+                    </p>
+                    
+                </div>
+
+                <!--========================================================
+                BUSCADOR
+                =========================================================-->
+
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+
+                    <input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+
+                    <span class="input-group-btn">
+
+                        <a href="#">
+
+                            <button class="btn btn-default backColor" type="submit">
+
+                                <i class="fa fa-search"></i>
+                            
+                            </button>
+                    
+                        </a>
+                    
+                    </span>
+                
+                </div>
+            
+            </div>
+
+            <!--========================================================
+            CARRITO DE COMPRA
+             =========================================================-->
+
+             <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+
+                <a href="#">
+
+                    <button class="btn btn-default pull-left backColor">
+
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                
+                    </button>
+                
+                </a>
+
+                <p>TU CESTA <span class="cantidadCesta"></span> <br> PESOS $ <span class="sumaCesta"></span></p>
+             
+             </div>
+        
+        </div>
+
+        <!--========================================================
+        CATEGORÍAS
+        =========================================================-->
+
+        <div class="col-xs-12 backColor" id="categorias">
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                
+                </ul>
+            
+            </div>
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                
+                </ul>
+            
+            </div>
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                
+                </ul>
+            
+            </div>
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategoria">Lorem Ipsum</a></li>
+                
+                </ul>
             
             </div>
         
