01221bd2ea9e3b4e59a43ccebdc74edb0b13bce6
Preprensa UndurragaTI <preprensaundurragati@Preprensas-MacBook-Pro-2.local>
2020-08-19 13:08:15 -0400

    Se cambio pesos por CLP

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -156,7 +156,7 @@ HEADER
                 
                 </a>
 
-                <p>TU CARRITO <span class="cantidadCesta"></span> <br> PESOS $ <span class="sumaCesta"></span></p>
+                <p>TU CARRITO <span class="cantidadCesta"></span> <br> CLP $ <span class="sumaCesta"></span></p>
              
              </div>
         
