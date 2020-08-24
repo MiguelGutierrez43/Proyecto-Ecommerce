e8a6ea6412a7b74ae9d891be323fa28f61e7f52a
Preprensa UndurragaTI <preprensaundurragati@Preprensas-MacBook-Pro-2.local>
2020-08-24 13:27:08 -0400

    Configuracion de plantilla dimanica 3ra parte

---
+++ frontend/vistas/js/plantilla.js
@@ -0,0 +1,10 @@
+/*=============================================
+PLANTILLA
+=============================================*/
+
+$.ajax({
+  url: "ajax/plantilla.ajax.php",
+  success: function (respuesta) {
+    console.log(respuesta);
+  },
+});
