34328aac0d927808a01b1689505f0a4ea6985f0f
Preprensa UndurragaTI <preprensaundurragati@Preprensas-MacBook-Pro-2.local>
2020-08-28 12:11:58 -0400

    Actualizando cabezote.php

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -1,198 +1,205 @@
-<!--========================================================
+<!--=====================================
 TOP
-=========================================================-->
+======================================-->
+
 <div class="container-fluid barraSuperior" id="top">
+	
+	<div class="container">
+		
+		<div class="row">
+	
+			<!--=====================================
+			SOCIAL
+			======================================-->
 
-    <div class="container">
+			<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+				
+				<ul>	
 
-        <div class="row">
+					<?php
 
-            <!--========================================================
-            SOCIAL
-            =========================================================-->
+					$social = ControladorPlantilla::ctrEstiloPlantilla();
 
-            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
-        
-                <ul>
-                    <?php
-                        $social = ControladorPlantilla::ctrEstiloPlantilla();
+					$jsonRedesSociales = json_decode($social["redesSociales"],true);		
 
-                        $jsonRedesSociales = json_decode($social["redesSociales"],true);
+					foreach ($jsonRedesSociales as $key => $value) {
 
-                        foreach($jsonRedesSociales as $key => $value){
-
-                            echo '<li>
+						echo '<li>
 								<a href="'.$value["url"].'" target="_blank">
 									<i class="fa '.$value["red"].' redSocial '.$value["estilo"].'" aria-hidden="true"></i>
 								</a>
 							</li>';
-                        }
-                    ?>
-                    
-                </ul>
+					}
 
-            </div>
+					?>
+			
+				</ul>
 
-            <!--========================================================
-            REGISTRO
-            =========================================================-->
+			</div>
 
-            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+			<!--=====================================
+			REGISTRO
+			======================================-->
 
-                <ul>
-                    <li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
-                    <li>|</li>
-                    <li><a href="#modalRegistro" data-toggle="modal">Crear una Cuenta</a></li>
-                </ul>
-            
-            </div>
-        
-        </div>
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+				
+				<ul>
+					
+					<li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+					<li>|</li>
+					<li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
 
-    </div>
+				</ul>
+
+			</div>	
+
+		</div>	
+
+	</div>
 
 </div>
 
-<!--========================================================
+<!--=====================================
 HEADER
-=========================================================-->
+======================================-->
+
 <header class="container-fluid">
+	
+	<div class="container">
+		
+		<div class="row" id="cabezote">
 
-    <div class="container">
+			<!--=====================================
+			LOGOTIPO
+			======================================-->
+			
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+				
+				<a href="#">
+						
+					<img src="http://localhost/backend/<?php echo $social["logo"]; ?>" class="img-responsive">
 
-        <div class="row" id="cabezote">
+				</a>
+				
+			</div>
 
-            <!--========================================================
-            HEADER
-            =========================================================-->
+			<!--=====================================
+			BLOQUE CATEGORÍAS Y BUSCADOR
+			======================================-->
 
-            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
-                
-                <a href="#">
+			<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+					
+				<!--=====================================
+				BOTÓN CATEGORÍAS
+				======================================-->
 
-                    <img src="http://localhost/undurraga-ti/Proyecto-Ecommerce/backend/<?php echo $social["logo"]; ?>" class="img-responsive">
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+					
+					<p>CATEGORÍAS
+					
+						<span class="pull-right">
+							<i class="fa fa-bars" aria-hidden="true"></i>
+						</span>
+					
+					</p>
 
-                </a>
+				</div>
 
-            </div>
+				<!--=====================================
+				BUSCADOR
+				======================================-->
+				
+				<div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+					
+					<input type="search" name="buscar" class="form-control" placeholder="Buscar...">	
 
-            <!--========================================================
-            BLOQUE CATEGORÍA Y BUSCADOR
-            =========================================================-->
+					<span class="input-group-btn">
+						
+						<a href="#">
 
-            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+							<button class="btn btn-default backColor" type="submit">
+								
+								<i class="fa fa-search"></i>
 
-                <!--========================================================
-                BOTÓN CATEGORÍA
-                =========================================================-->
+							</button>
 
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+						</a>
 
-                    <p>CATEGORÍAS
+					</span>
 
-                        <span class="pull-right">
-                            <i class="fa fa-bars" aria-hidden="true"></i>
-                        </span>
+				</div>
+			
+			</div>
 
-                    </p>
-                    
-                </div>
+			<!--=====================================
+			CARRITO DE COMPRAS
+			======================================-->
 
-                <!--========================================================
-                BUSCADOR
-                =========================================================-->
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+				
+				<a href="#">
 
-                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+					<button class="btn btn-default pull-left backColor"> 
+						
+						<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+					
+					</button>
+				
+				</a>	
 
-                    <input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+				<p>TU CESTA <span class="cantidadCesta">3</span> <br> USD $ <span class="sumaCesta">20</span></p>	
 
-                    <span class="input-group-btn">
+			</div>
 
-                        <a href="#">
+		</div>
 
-                            <button class="btn btn-default backColor" type="submit">
+		<!--=====================================
+		CATEGORÍAS
+		======================================-->
 
-                                <i class="fa fa-search"></i>
-                            
-                            </button>
-                    
-                        </a>
-                    
-                    </span>
-                
-                </div>
-            
-            </div>
+		<div class="col-xs-12 backColor" id="categorias">
 
-            <!--========================================================
-            CARRITO DE COMPRA
-             =========================================================-->
+			<?php
 
-             <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+				$item = null;
+				$valor = null;
 
-                <a href="#">
+				$categorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
 
-                    <button class="btn btn-default pull-left backColor">
+				foreach ($categorias as $key => $value) {
 
-                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                
-                    </button>
-                
-                </a>
+					echo '<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+							
+							<h4>
+								<a href="'.$value["ruta"].'" class="pixelCategorias">'.$value["categoria"].'</a>
+							</h4>
+							
+							<hr>
 
-                <p>TU CARRITO <span class="cantidadCesta"></span> <br> CLP $ <span class="sumaCesta"></span></p>
-             
-             </div>
-        
-        </div>
+							<ul>';
 
-        <!--========================================================
-        CATEGORÍAS
-        =========================================================-->
+							$item = "id_categoria";
 
-        <div class="col-xs-12 backColor" id="categorias">
+							$valor = $value["id"];
 
-        <?php
+							$subcategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
+							
+							foreach ($subcategorias as $key => $value) {
+									
+									echo '<li><a href="'.$value["ruta"].'" class="pixelSubCategorias">'.$value["subcategoria"].'</a></li>';
+								}	
+								
+							echo '</ul>
 
-            $item = null;
-            $valor = null;
-                        
+						</div>';
+				}
 
-            $categorias = ControladorProductos::ctrMostrarCategorias($item, $valor);
+			?>	
 
-            foreach ($categorias as $key => $value) {
+		</div>
 
-                echo '<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
-
-                        <h4>
-                            <a href="'.$value["ruta"].'" class="pixelCategorias">'.$value["categoria"].'</a>
-                        </h4>
-
-                        <hr>
-
-                        <ul>';
-
-                        $item = "id_categoria";
-
-                        $valor = $value["id"];
-
-                        $subcategorias = ControladorProductos::ctrMostrarSubCategorias($item, $valor);
-
-                        foreach ($subcategorias as $key => $value) {
-                            
-                            echo'<li><a href="'.$value["ruta"].'" class="pixelSubCategoria">'.$value["subcategoria"].'</a></li>';
-                        }
-
-                        echo'
-
-                        </ul>
-            
-                    </div>';
-            }
-        ?>
-        
-        </div>
-
-    </div>
+	</div>
 
 </header>
+
+
