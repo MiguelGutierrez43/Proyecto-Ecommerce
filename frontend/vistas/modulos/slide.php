355c461a98d32327667f20bc721cf0caa3b1026d
Preprensa UndurragaTI <preprensaundurragati@Preprensas-MacBook-Pro-2.local>
2020-09-02 11:41:00 -0400

    Maquetando el slide3

--- frontend/vistas/modulos/slide.php
+++ frontend/vistas/modulos/slide.php
@@ -78,6 +78,74 @@ SLIDESHOW
                 </div>
 
             </li>
+
+            <!-- SLIDE 3 -->
+            
+            <li>
+            
+                <img src="http://localhost/undurraga-ti/Proyecto-Ecommerce/backend/vistas/img/slide/slide3/fondo2.jpg">
+
+                <div class="slideOpciones slideOpcion2">
+                
+                    <img class="imgProducto" src="http://localhost/undurraga-ti/Proyecto-Ecommerce/backend/vistas/img/slide/slide3/iphone.png">
+
+                    <div class="textosSlide">
+                    
+                        <h1>15% de Descuento</h1>
+
+                        <h2>Tarjetas de Presentación</h2>
+
+                        <h3>Full Color</h3>
+
+                        <a href="#">
+                        
+                            <button class="btn btn-default backColor text-uppercase">
+
+                            VER PRODUCTO <span class="fa fa-chevron-right"></span>
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
+
+            <!-- SLIDE 4 -->
+            
+            <li>
+            
+                <img src="http://localhost/undurraga-ti/Proyecto-Ecommerce/backend/vistas/img/slide/slide4/fondo3.jpg">
+
+                <div class="slideOpciones slideOpcion1">
+                
+                    <img class="imgProducto" src="">
+
+                    <div class="textosSlide">
+                    
+                        <h1>15% de Descuento</h1>
+
+                        <h2>Tarjetas de Presentación</h2>
+
+                        <h3>Full Color</h3>
+
+                        <a href="#">
+                        
+                            <button class="btn btn-default backColor text-uppercase">
+
+                            VER PRODUCTO <span class="fa fa-chevron-right"></span>
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
         
         </ul>
 
