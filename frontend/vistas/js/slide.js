ace85eb9395bce413747f8bd7e282702975513ec
Preprensa UndurragaTI <preprensaundurragati@MacBook-Pro-Miguel.local>
2020-12-03 16:04:26 -0300

    Confifurando slide.js2

--- frontend/vistas/js/slide.js
+++ frontend/vistas/js/slide.js
@@ -8,6 +8,17 @@ PAGINACIÓN
 ======================================*/
 $("#paginacion li").click(function(){
 
-    item = $(this).attr("item");
-    console.log("item", item);
+    item = $(this).attr("item")-1;
+    
+    movimientoSlide(item);
 })
+
+/*=====================================
+MOVIMIENTO SLIDE
+======================================*/
+
+function movimientoSlide(item) {
+
+    $("#slide ul").animate({"left" : item * -100 + "%"}, 1000);
+    
+}
