93993f52a027c93161b23e6f62eade705c88535f
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-06-04 11:56:38 -0400

    Maquetando pagina de inicio 2

--- frontend/vistas/modulos/destacados.php
+++ frontend/vistas/modulos/destacados.php
@@ -4,17 +4,16 @@ BANNER
 
 <figure class="banner">
 
+	<img src="http://localhost/undurraga-ti/Proyecto-Ecommerce/backend/vistas/img/banner/default.jpg" class="img-responsive" width="100%">	
 
-    <img src="http://localhost/undurraga-ti/backend/vistas/img/banner/default.jpg" class="img-responsive" width="100%">
+	<div class="textoBanner textoDer">
+		
+		<h1 style="color:#fff">OFERTAS ESPECIALES</h1>
 
-    <div class="textoBanner textoDer">
+		<h2 style="color:#fff"><strong>50% off</strong></h2>
 
-        <h1 style="color:#fff">OFERTAS ESPECIALES</h1>
+		<h3 style="color:#fff">Termina el 31 de Octubre</h3>
 
-        <h2 style="color:#fff"><strong>50% off</strong></h2>
-
-        <h3 style="color:#fff">Termina el 31 de Octubre</h3>
-    
-    </div>
+	</div>
 
 </figure>
